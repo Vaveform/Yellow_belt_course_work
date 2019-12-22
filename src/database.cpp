a0405b0771df480637ed42e0068bc50bc9b3b3f0
Max <postas123744@gmail.com>
2019-12-23 02:10:53 +0300

    Start to make database.cpp

--- src/database.cpp
+++ src/database.cpp
@@ -1,5 +1,59 @@
 #include "database.h"
 
+Entry::Entry(const Date& d, const string& e)
+{
+	date = d;
+	activity = e;
+}
+
+Date Entry::GetDate() const
+{
+	return date;
+}
+
+string Entry::GetEvent() const
+{
+	return activity;
+}
+
+ostream& operator<<(ostream& stream, const Entry& pair){
+	stream << pair.GetDate() << " " << pair.GetEvent();
+	return stream;
+}
+
+void Database::Add(const Date& date, const string& event)
+{
+	auto not_exist = repeated[date].insert(event).second;
+	if(not_exist){
+		data[date].push_back(event);
+	}
+	else{
+		return;
+	}
+}
+
+void Database::Print(ostream& output) const
+{
+	for(auto it_dict = data.begin(); it_dict != data.end(); ++it_dict)
+	{
+		for(auto it_vec = it_dict->second.begin(); it_vec!=it_dict->second.end(); ++it_vec){
+			cout << Entry(it_dict->first, *it_vec) << endl;
+		}
+	}
+}
+
+Entry Database::Last(const Date& date) const
+{
+	if(date < data.begin()->first)
+	{
+		throw invalid_argument("Event");
+	}
+	auto last_pair = prev(data.upper_bound(date));
+	return Entry(last_pair->first, last_pair->second.back());
+}
+
+
+
 
 
 
--- src/database.h
+++ src/database.h
@@ -1,25 +1,38 @@
 #pragma once
 
-//#include "date.h"
-//#include <iostream>
-//#include <string>
-//#include <map>
-//#include <vector>
-//#include <utility>
-//
-//struct Entry{
-//	Date date;
-//	string activity;
-//	ostream& operator<< (ostream& stream);
-//};
-//
-//class Database
-//{
-//private:
-//	map<Date, vector<string>> data;
-//public:
-//	void Add(const string& date, const string& event);
-//	void Print(ostream& output) const;
-//	string Last(const string& date) const;
+#include "date.h"
+#include <iostream>
+#include <string>
+#include <map>
+#include <set>
+#include <vector>
+#include <utility>
+#include <stdexcept>
+#include <exception>
+
+using namespace std;
+
+struct Entry{
+private:
+	Date date;
+	string activity;
+public:
+	Entry(const Date& d, const string& e);
+	Date GetDate() const;
+	string GetEvent() const;
+	friend ostream& operator<<(ostream& stream, const Entry& pair);
+};
+
+ostream& operator<<(ostream& stream, const Entry& pair);
+
+class Database
+{
+private:
+	map<Date, vector<string>> data;
+	map<Date, set<string>> repeated;
+public:
+	void Add(const Date& date, const string& event);
+	void Print(ostream& output) const;
+	Entry Last(const Date& date) const;
 //	vector<Entry> FindIf()
-//};
+};
